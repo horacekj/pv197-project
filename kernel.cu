596b3499446eb1edc1b38061ca201a1919a6ac44
Jan Horacek <me@apophis.cz>
2016-10-30 00:53:45 +0200

    Divide both arrays in one kernel.

--- kernel.cu
+++ kernel.cu
@@ -3,6 +3,9 @@
 #define BLOCK_WIDTH  32
 #define BLOCK_HEIGHT 32
 
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
 __global__ void mySolve(const int *matrix, float *col, float *row, const int height, const int width) {
 	size_t laneID = threadIdx.x & 31;
 
@@ -26,15 +29,17 @@ __global__ void mySolve(const int *matrix, float *col, float *row, const int hei
 	atomicAdd(&col[x], vertical);
 }
 
-__global__ void myAverage(float *array, const int divisor) {
+__global__ void bothAverage(float* first, float *second, const int firstLen, const int secondLen) {
 	int index = blockDim.x * blockIdx.x + threadIdx.x;
-	array[index] = array[index] / divisor;
+	if (index < firstLen)
+		first[index] = first[index] / secondLen;
+	if (index < secondLen)
+		second[index] = second[index] / firstLen;
 }
 
 void solveGPU(const int *matrix, float *avg_stud, float *avg_que, const int students, const int questions) {
-	cudaMemset(avg_que, 0, questions*sizeof(float));
-	cudaMemset(avg_stud, 0, students*sizeof(float));
+	cudaMemset(avg_que, 0, questions*sizeof(*avg_que));
+	cudaMemset(avg_stud, 0, students*sizeof(*avg_stud));
 	mySolve<<<dim3(questions/32, students/32), dim3(32, 32/N_ITERATIONS)>>>(matrix, avg_que, avg_stud, students, questions);
-	myAverage<<<dim3(questions/32), dim3(32, 1)>>>(avg_que, students);
-	myAverage<<<dim3(students/32), dim3(32, 1)>>>(avg_stud, questions);
+	bothAverage<<<dim3(MAX(students,questions)/32), dim3(32, 1)>>>(avg_que, avg_stud, questions, students);
 }
