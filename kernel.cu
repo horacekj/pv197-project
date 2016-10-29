7ed883090fd8c2c209f8108c983f5a26d90dee98
Jan Horacek <me@apophis.cz>
2016-10-29 19:14:03 +0200

    Add block dimension constant.
     * 16000 mevals.

--- kernel.cu
+++ kernel.cu
@@ -1,4 +1,4 @@
-#define N_ITERATIONS 32
+#define N_ITERATIONS 8
 
 #define BLOCK_WIDTH  32
 #define BLOCK_HEIGHT 32
@@ -7,19 +7,19 @@ __global__ void mySolve(const int *matrix, float *col, float *row, const int hei
 	size_t laneID = threadIdx.x & 31;
 
 	size_t x = blockDim.x * blockIdx.x + threadIdx.x; // absolutni X pozice v matici
-	size_t y = blockIdx.y; // id bloku y
+	size_t y = 32 * blockIdx.y + N_ITERATIONS * threadIdx.y; // absolutni Y pozice pocatku pocitani
 
-	size_t idx = 32 * y * width + x;
+	size_t idx = y * width + x;
 
 	float vertical = 0;
 
-	for(int i = 0; i < 32; ++i) {
+	for(int i = 0; i < N_ITERATIONS; ++i) {
 		float v = matrix[idx];
 		vertical += v;
 		for(unsigned w = 16; w >= 1; w >>= 1)
 			v += __shfl_down(v, w);
 		if(laneID == 0)
-			atomicAdd(&row[y*32+i], v);
+			atomicAdd(&row[y+i], v);
 		idx += width;
 	}
 
@@ -34,7 +34,7 @@ __global__ void myAverage(float *array, const int divisor) {
 void solveGPU(const int *matrix, float *avg_stud, float *avg_que, const int students, const int questions) {
 	cudaMemset(avg_que, 0, questions*sizeof(float));
 	cudaMemset(avg_stud, 0, students*sizeof(float));
-	mySolve<<<dim3(questions/32, students/32), dim3(32, 1)>>>(matrix, avg_que, avg_stud, students, questions);
+	mySolve<<<dim3(questions/32, students/32), dim3(32, 32/N_ITERATIONS)>>>(matrix, avg_que, avg_stud, students, questions);
 	myAverage<<<dim3(questions/32), dim3(32, 1)>>>(avg_que, students);
 	myAverage<<<dim3(students/32), dim3(32, 1)>>>(avg_stud, questions);
 }
